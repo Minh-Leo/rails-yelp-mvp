7718695a9d7588557914e1605717ec01073b42d0
minh bui <tranduyminh.bui@gmail.com>
2020-02-19 15:23:58 +1100

    finish basci features for yelp simple

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_000755) do
+ActiveRecord::Schema.define(version: 2020_02_19_003553) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_000755) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category: 'chinese',
+    phone_number: '0203040506'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category: 'italian',
+    phone_number: '0203040506'
+  },
+  {
+    name:         'McDonald',
+    address:      '56A Franklin st VIC',
+    description:  'Shitty hamburger and chicken nugget.',
+    category: 'belgian',
+    phone_number: '0203040506'
+  },
+  {
+    name:         'KFC',
+    address:      '128 Bottom st VIC',
+    description:  'Better chicken than McDonald, still need more coke to digest them all.',
+    category: 'french',
+    phone_number: '0203040506'
+  },
+  {
+    name:         'Cheesy Pie',
+    address:      '32 St Kilda st VIC',
+    description:  'Mix cheese and pie together, not a bad ideas.',
+    category: 'japanese',
+    phone_number: '0203040506'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
